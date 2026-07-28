24967838642e3ddb48388c90bafc9022111e5fe8
habibishrawanshah-ctrl <habibishrawanshah@gmail.com>
2026-07-28 17:28:28 +0545

    Move api/ to git root so Vercel detects serverless functions

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'pharmacy_ecommerce'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pharmacy_ecommerce.settings')
 
 from django.core.wsgi import get_wsgi_application

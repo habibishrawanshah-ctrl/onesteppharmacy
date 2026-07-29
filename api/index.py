7d394c4553d433eb6e73716359ec9d1fb80fa575
habibishrawanshah-ctrl <habibishrawanshah@gmail.com>
2026-07-29 09:35:44 +0545

    fix: use application instead of app, disable Vercel Django detection

--- api/index.py
+++ api/index.py
@@ -5,4 +5,4 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pharmacy_ecommerce.settings')
 
 from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()
+application = get_wsgi_application()

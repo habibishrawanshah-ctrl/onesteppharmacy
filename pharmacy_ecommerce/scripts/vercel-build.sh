a482c11909beae9b8a309bb5e7d7200070285950
habibishrawanshah-ctrl <habibishrawanshah@gmail.com>
2026-07-29 17:06:11 +0545

    fix: run migrate in Vercel build to sync DB schema

--- pharmacy_ecommerce/scripts/vercel-build.sh
+++ pharmacy_ecommerce/scripts/vercel-build.sh
@@ -2,6 +2,7 @@
 set -e
 cd pharmacy_ecommerce
 pip install --break-system-packages -r requirements.txt
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 mkdir -p ../public/static
 cp -r staticfiles/. ../public/static/
